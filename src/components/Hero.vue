b51c9cdfdbec131a768a76cea6fcdbb7edde0c15
MarcoPorcaro1987 <marcoporcaro87@gmail.com>
2021-09-28 15:01:41 +0200

    Aggiunti

---
+++ src/components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <img
+  src="https://cdn.pixabay.com/photo/2017/08/28/19/15/background-2691002_960_720.jpg"
+  alt="">
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+    img {
+        height: 400px;
+        width: 100%;
+    }
+</style>
